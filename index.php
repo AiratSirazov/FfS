078b86fa7b303186c9b191c861e1f8b4e4299a41
Airat Sirazov <sirazovairat@yandex.ru>
2016-11-01 00:12:19 +0300

    Test commit

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <!-- Окно предупреждения -->
     <div id="warning" hidden>Bum</div>
     <!-- Конец предупреждения -->
-    Hello world amigo
     <?php include_once dirname(__FILE_).'/views/login_regist.php';?>
 </div>
 <?php include_once dirname(__FILE__).'/views/parts/footer.php'; ?>
